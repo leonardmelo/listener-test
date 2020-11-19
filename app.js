a876fc851538ceb88cac8dea76b0d03fdf2d5778
Leonardo Melo <lcmelo@koerich.com.br>
2020-11-19 17:04:20 -0300

    Implementado metodo Post, para receber os updates

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/update/payment', (req, res) =>{
+  console.log(req.body)
+  res.send("Ok")
 })
 
 app.listen(port, () => {
