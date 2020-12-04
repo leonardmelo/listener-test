e30a2af0a92b485290f815f113763f4934ae9712
Leonardo Melo <lcmelo@koerich.com.br>
2020-12-04 13:51:53 -0300

    Alterada forma de imprimir os dados

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/update/payment', (req, res) =>{
-  console.log(req.body)
-  console.log(req.body.data.paymentMethods)
+  console.log(JSON.stringify(req.body))
   res.send("Ok")
 })
 
